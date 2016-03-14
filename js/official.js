fc4fc1d27fcb963b2c9337f7676377081660647f
shivateja-madipalli <madipallishivateja@gmail.com>
2016-03-14 04:32:32 -0700

    Additions to Official page

--- js/official.js
+++ js/official.js
@@ -1,7 +1,10 @@
 // official js
 
+var serverUrl = "https://mighty-crag-35032.herokuapp.com/shivatejam/";
+
 $(document).ready(function(){
     getDescription();   
+    getGitData();
     
 });
 
@@ -10,7 +13,7 @@ function getDescription() {
           var finalData;
       $.ajax({
           //https://mighty-crag-35032.herokuapp.com/shivatejam/home/Description
-            url: 'https://mighty-crag-35032.herokuapp.com/shivatejam/home/Description',
+            url: serverUrl + 'home/Description',
             type: 'GET',
             success: function(data) {
                 if(data != null) {
@@ -54,6 +57,85 @@ function getDescription() {
             });          
       }
       catch(err) {
-          console.log("error have been throughed "+ err);
+          console.log("error is caught "+ err);
+      }
+  }
+  
+  var anchortagclassname = "git_proj_names_a_class";  
+  var tdrightclassname = "git_proj_names_td_right_class";  
+  var tdleftclassname = "git_proj_names_td_left_class";  
+  var trclassname ="git_proj_rows_class";
+  var descriptiontdrightclassname = "git_proj_description_right_class";
+  var descriptiontdleftclassname = "git_proj_description_left_class";
+  
+  function getGitData() {
+      try {
+          
+            $.ajax({
+          //https://mighty-crag-35032.herokuapp.com/shivatejam/home/Description
+            url: serverUrl + 'githubrepos',
+            type: 'GET',
+            success: function(data) {
+                if(data != null) {
+                        console.log("Git Data is not null");
+                    }
+                },
+            complete: function(data) {
+                console.log("git data.status: "+data.status);
+                if(data.status == 200) {
+                    dataAsString = JSON.stringify(data);
+                    console.log(dataAsString);
+                    // jsonData = JSON.parse(JSON.stringify(data.responseJSON.data));
+                    
+                    jsonData = data.responseJSON.data;
+                    console.log("git full data: " + jsonData);
+                    //official_body_div3_table
+                    //official_body_div3_table_row1
+                    var htmlContent;
+                    console.log("data.length: " + jsonData.length);
+                    for(i=0; i < jsonData.length; i++) 
+                    {                        
+                        htmlContent += "<tr class="+trclassname+">";
+                            if(i % 2 == 0) {
+                                htmlContent += "<td class="+tdrightclassname+">";    
+                                htmlContent += "<a src = "+jsonData[i].html_url+"class="+anchortagclassname+">"+jsonData[i].name+"</a>";
+                                htmlContent += "</td>";
+                                
+                                htmlContent += "<td class="+descriptiontdleftclassname+">";
+                                htmlContent += "<p>description : "+jsonData[i].description+", created_at : " +jsonData[i].created_at+", language : "+jsonData[i].language+"</p>";
+                                htmlContent += "</td>";
+                            }
+                            else {
+                                htmlContent += "<td class="+descriptiontdrightclassname+">";
+                                
+                                htmlContent += "<p>description : "+jsonData[i].description+", created_at : " +jsonData[i].created_at+", language : "+jsonData[i].language+"</p>";
+                                htmlContent += "</td>";
+                                
+                                htmlContent += "<td class="+tdleftclassname+">";
+                                htmlContent += "<a src = "+jsonData[i].html_url+"class="+anchortagclassname+">"+jsonData[i].name+"</a>";
+                                htmlContent += "</td>";
+                            }
+                            
+                            
+                        htmlContent += "</tr>";                        
+                    }
+                    //actual Html Content Of the Row
+                    var actualHtml = $('#official_body_div3_table_row1').html();
+                    $('#official_body_div3_table_row1').html(actualHtml + htmlContent);
+                }
+                else {
+                    console.log("git Something is wrong");
+                    }
+                },
+            error : function(err) {
+                    console.log("git error is: " + err.error);
+                    window.alert("As a Developer I do hate these issues, an error :( please refresh the page");
+                },
+            });          
+    
+          
+      }
+      catch(err){
+          console.log("error is caught"+ err);
       }
   }
